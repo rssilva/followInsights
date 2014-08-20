dec8b9c4068aec30add2500fc99f81f35975ae69
rssilva <rafaelspdasilva@gmail.com>
2014-08-19 23:58:51 -0300

    changing method names, adding some tests trying to filter if there are too many nodes

--- models/ChordGraph.js
+++ models/ChordGraph.js
@@ -1,22 +1,27 @@
 var _ = require('lodash');
+var User = new require('../models/User')();
 
 var chordGraph = {
 	nodes: [],
-	insertUser: function (username, firstLevel) {
+	insertQueryUser: function (username, firstLevel) {
 		this.nodes.push({
 			name: username,
 			imports: _.pluck(firstLevel, 'follows')
 		})
 	},
 
-	parseData: function (username, firstLevel, secondLevel) {
+	parseData: function (username, levels) {
 		var that = this;
 		var toInsert = [];
 		var byUser = [];
 		var loginsByUser = [];
+		var firstLevel = levels[0].fromFollowers;
+		var secondLevel = levels[1].fromFollowers;
 
 		this.nodes = [];
-		this.insertUser(username, firstLevel);
+		this.insertQueryUser(username, firstLevel);
+
+		console.log('lenghts', firstLevel.length, secondLevel.length)
 
 		//iterates through the first level users
 		_.each(firstLevel, function (followedFirstLevel) {
@@ -25,11 +30,11 @@ var chordGraph = {
 			//It's necessary because the results.secondLevel is an array with objects
 			//but is not filtered
 			byUser = _.where(secondLevel, {login: followedFirstLevel.follows})
-
+			//console.log(followedFirstLevel.follows)
 			//get just the property 'follows' on 'byUser' array
 			loginsByUser = _.pluck(byUser, 'follows');
 
-			//avoids duplicated entrys from database (yes, exactly)
+			//avoids duplicated entrys from database (yes, I know, that sucks)
 			loginsByUser = _.union(loginsByUser);
 
 			that.nodes.push({
@@ -40,6 +45,18 @@ var chordGraph = {
 
 		toInsert = this.findMissingNodes();
 		this.insertMissingNodes(toInsert);
+
+		console.log('nodes length ' , this.nodes.length)
+
+		var user = {};
+
+		_.each(this.nodes, function (node) {
+			
+			if (node.name !== username) {
+				user = User.schema.methods.findUserOnLevels(levels, node.name);
+				//console.log(user.login)
+			}
+		})
 
 		return this.nodes;
 	},
